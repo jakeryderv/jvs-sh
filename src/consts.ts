835047391ab896229aba98c671d659beb273556b
Jake Van Slyke <jakervanslyke@gmail.com>
2026-09-11 22:27:35 -0500

    Add the LinkedIn link to the intro

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 export const SITE_TITLE = 'jvs.sh';
 export const SITE_DESCRIPTION = 'Jake Van Slyke';
 export const GITHUB_URL = 'https://github.com/jakeryderv';
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/jakeryderv/';
 
 // Top-level pages, in nav order. Adding a page means adding a route and one entry here.
 export const NAV = [
